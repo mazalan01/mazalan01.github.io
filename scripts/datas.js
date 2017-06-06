ae9990466488919bce2999d2a4bbddd7e42a4a25
mazalan01 <mazalan01@gmail.com>
2017-06-06 08:43:17 +0200

    more cities

--- scripts/datas.js
+++ scripts/datas.js
@@ -19,6 +19,25 @@ function data_make_array(){
 	data_city_loaction.push(["frankfurt",50.1109221,8.682126700000026,"Németország"]);
 	data_city_loaction.push(["dortmund",51.5135872,7.465298100000041,"Németország"]);
 	data_city_loaction.push(["berlin",52.52000659999999,13.404953999999975,"Németország"]);
+	data_city_loaction.push(["munich",48.1351253,11.581980599999952,"Németország"]);
+	data_city_loaction.push(["madrid",40.4167754,-3.7037901999999576,"Spanyolország"]);
+	data_city_loaction.push(["oslo",59.9138688,10.752245399999993,"Norvégia"]);
+	data_city_loaction.push(["paris",48.856614,2.3522219000000177,"Franciaország"]);
+	data_city_loaction.push(["stavanger",58.9699756,5.733107300000029,"Norvégia"]);
+	data_city_loaction.push(["aberdeen",57.149717,-2.094278000000031,"Egyesült Királyság"]);
+	data_city_loaction.push(["warsaw",52.2296756,21.012228700000037,"Lengyelország"]);
+	data_city_loaction.push(["dubai",25.2048493,55.270782800000006,"Egyesült Arab Emírségek"]);
+	data_city_loaction.push(["sydney",-33.8688197,151.20929550000005,"Ausztrália"]);
+	data_city_loaction.push(["tel aviv",32.0852999,34.78176759999997,"Izrael"]);
+	data_city_loaction.push(["atlanta",33.7489954,-84.3879824,"Egyesült Államok"]);
+	data_city_loaction.push(["tallahassee",30.4382559,-84.28073289999998,"Egyesült Államok"]);
+	data_city_loaction.push(["chicago",41.8781136,-87.62979819999998,"Egyesült Államok"]);
+	data_city_loaction.push(["prague",50.0755381,14.43780049999998,"Csehország"]);
+	data_city_loaction.push(["rome",41.9027835,12.496365500000024,"Olaszország"]);
+	data_city_loaction.push(["bonn",50.73743,7.098206800000071,"Németország"]);
+	data_city_loaction.push(["copenhagen",55.6760968,12.568337100000008,"Dánia"]);
+	data_city_loaction.push(["melbourne",-37.8136276,144.96305759999996,"Ausztrália"]);
+	data_city_loaction.push(["singapore",1.352083,103.81983600000001,"Szingapúr"]);
 }
 
 function data_is_city(name) {
